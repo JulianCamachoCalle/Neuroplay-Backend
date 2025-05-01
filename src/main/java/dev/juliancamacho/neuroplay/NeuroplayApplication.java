7c1e6a171522e87739d3094bcf46128ff6b9306f
JulianCamachoCalle <camachocallejulian@gmail.com>
2025-05-01 17:11:53 -0500

    Backend - Project Structure

---
+++ src/main/java/dev/juliancamacho/neuroplay/NeuroplayApplication.java
@@ -0,0 +1,13 @@
+package dev.juliancamacho.neuroplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/juliancamacho/neuroplay/NeuroplayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juliancamacho.neuroplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
